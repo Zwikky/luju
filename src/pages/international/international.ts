033bbcdab8822bb63ec60fbdb6bcee9072af58ea
Zwakele Mkhatshwa <zwikky@gmail.com>
2017-11-16 05:07:52 +0200

    Luju Initialize

--- src/pages/international/international.ts
+++ src/pages/international/international.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, AlertController, NavParams } from 'ionic-angular';
 
 /**
  * Generated class for the InternationalPage page.
@@ -15,7 +15,30 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class InternationalPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public alertCtrl: AlertController, public navParams: NavParams) {
+  }
+
+  buyTrack() {
+    let alert = this.alertCtrl.create({
+      title: 'Confirm purchase',
+      message: 'Do you want to buy this track?',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Buy',
+          handler: () => {
+            console.log('Buy clicked');
+          }
+        }
+      ]
+    });
+    alert.present();
   }
 
   ionViewDidLoad() {
